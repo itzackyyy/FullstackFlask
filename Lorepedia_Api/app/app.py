9250c3fa77549bbccc0bf546bcfe8d04d6487874
itzackyyy <brun.soto@duocuc.cl>
2025-10-24 16:17:05 -0300

    Actualización v2.2.3 del proyecto, se han hecho arreglos íncimos en "Personajes.html" redireccionando de forma correcta el "Home" / <-

--- Lorepedia_Api/app/app.py
+++ Lorepedia_Api/app/app.py
@@ -1,40 +1,16 @@
 from flask import Flask, render_template, redirect, request, Response, session, jsonify
 from livereload import Server as sv
 import pymysql as sqlxd
-import os
-from werkzeug.security import generate_password_hash
-from flask_cors import CORS
-
-
-# #Información de login hacia la base de datos.
-# DB_CONFIG = {
-#     'host': os.getenv('DB_HOST', '127.0.0.1'),
-#     'user': os.getenv('DB_USER','lore_user'),
-#     'password': os.getenv('DB_PASS','LoreDB12$'),
-#     'database': os.getenv('DB_NAME','lorepedia_db'),
-#     'cursorclass': sqlxd.cursors.DictCursor,
-#     'charset': 'utf8mb4'
-# }
-
-#Conexión hacia la base de datos desde Flask hacia MySQL
-# def db_con():
-#     return sqlxd.connect(
-#         host=DB_CONFIG['host'],
-#         user=DB_CONFIG['user'],
-#         password=DB_CONFIG['password'],
-#         database=DB_CONFIG['database'],
-#         cursorclass=DB_CONFIG['cursorclass'],
-#         charset=DB_CONFIG['charset']
-#     )
-
+import pytest as pt
 
 
 app=Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route('/login')#endpoint login
 def login():
     return render_template('lorepedia/iniciarSesion.html')
+
 
 @app.route('/registrar')#endpoint para registrar
 def registrar():
